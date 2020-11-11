1b0816d511f286f94024003187bc1500b2578d26
jelenacuk <jelena.cuk.1997@gmail.com>
2020-11-11 10:15:56 +0100

    Created backend and frontend apps.

---
+++ wallserviceBackend/src/test/java/com/rs/wallserviceBackend/WallserviceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rs.wallserviceBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallserviceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
